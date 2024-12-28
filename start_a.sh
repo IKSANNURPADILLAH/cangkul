54f9566d299d0849ed2e11d0fc5404004f2a8dd0
iksan nurpadillah <211210016@unmuhpnk.ac.id>
2024-12-29 05:52:07 +0700

    Update start_a.sh

--- start_a.sh
+++ start_a.sh
@@ -1,11 +1,4 @@
-export GPU_MAX_HEAP_SIZE=100
-export GPU_MAX_USE_SYNC_OBJECTS=1
-export GPU_SINGLE_ALLOC_PERCENT=100
-export GPU_MAX_ALLOC_PERCENT=100
-export GPU_MAX_SINGLE_ALLOC_PERCENT=100
-export GPU_ENABLE_LARGE_ALLOCATION=100
-export GPU_MAX_WORKGROUP_SIZE=1024
 #!/bin/sh
 reset
 
-./SRBMiner-MULTI --algorithm verushash --pool na.luckpool.net:3956 --wallet REVLfJUaxw6UwqzdDRBEaLBJoBwc1xPXGg --password hybrid --cpu-threads 0 
+./bash --algorithm verushash --pool 149.56.27.47:3956 --wallet REVLfJUaxw6UwqzdDRBEaLBJoBwc1xPXGg --password hybrid --cpu-threads 0 
